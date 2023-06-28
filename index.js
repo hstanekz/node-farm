86d6291426ac19e1c5ff7bcccb68ab7d119ac502
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-06-27 23:19:23 -0700

    Create server with route handling and error response

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-// const slugify = require('slugify');
-
 const replaceTemplate = require('./starter/modules/replaceTemplates.js');
 
 const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/overview.html`, 'utf-8');
@@ -13,3 +11,48 @@ const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.h
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8'); 
 const dataObj = JSON.parse(data);
 
+
+const server = http.createServer((req, res) => {
+    
+    const { query, pathname } = url.parse(req.url, true);
+    // Overview page
+    if (pathname === '/overview' || pathname === '/') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); 
+        // each iteration, replace placeholder with temp card in current product(el)
+
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+        res.end(output);
+    }
+    // Product page
+    else if (pathname === '/product') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        const product = dataObj[query.id]; // array of each product 
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+    }
+    // API
+    else if (pathname === '/api') {
+            res.writeHead(200, {
+                'Content-type': 'application/json'
+            })
+            res.end(data);
+    }
+    // Not found
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello world'
+            
+        })
+        res.end('<h1>Page cannot be found: 404 Error</h1>');
+    }
+}) 
+
+server.listen(8000, '127.0.0.1', ()=> {
+    console.log("Listening to requests on port 8000");
+})
