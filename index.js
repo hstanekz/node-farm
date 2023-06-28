0c6547b0ddfd214ff83af3782e9d003557a54632
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-06-27 23:55:51 -0700

    Test index.js file for correct URL linking

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const replaceTemplate = require('./starter/modules/replaceTemplates.js');
+const replaceTemplate = require('./modules/replaceTemplates.js');
 
-const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/overview.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/product.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 
-const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8'); 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); 
 const dataObj = JSON.parse(data);
 
 
